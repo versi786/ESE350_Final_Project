4af319956d6f214c1a87df762360a333d77b5ff1
Aasif Versi <versia@seas.upenn.edu>
2015-04-21 23:10:33 -0400

    final version of code for demo day

--- MBed/rest_chewing_sliding_window_test.cpp
+++ MBed/rest_chewing_sliding_window_test.cpp
@@ -3,10 +3,12 @@
 #include <map>
 #include "math.h"
 using namespace std;
-#define EMG_BUFFER_LENGTH 3000
+#define EMG_BUFFER_LENGTH 2000
+#define CHEW_BUFFER_LENGTH 22
 
 DigitalOut myled(LED1);
 DigitalOut myled2(LED2);
+DigitalOut myled3(LED3);
 DigitalOut motor(p5);
 Serial pc(USBTX, USBRX);
 Timer timer;
@@ -17,10 +19,36 @@ AnalogIn emg_pin(p20);
 int emg_buffer[EMG_BUFFER_LENGTH];
 int emg_position;
 
+int chew_buffer[CHEW_BUFFER_LENGTH]; //3 seconds of data;
+int chew_position;
+
 void emg_isr() {
     emg_buffer[emg_position] = emg_pin.read_u16();
     emg_position = (emg_position + 1) % EMG_BUFFER_LENGTH;
     // pc.printf("%d\r\n", emg_pin.read_u16());
+}
+
+void chew_place(int classification) {
+    chew_buffer[chew_position] = classification;
+    chew_position = (chew_position + 1) % CHEW_BUFFER_LENGTH;
+}
+
+void chew_test(){
+    int count = 0;
+    for(int i = 0; i < CHEW_BUFFER_LENGTH; i++){
+        count += chew_buffer[i];
+    }
+    if(count > 8){ // 3 chews, because chews span over multiple windows
+        emg.detach();
+        motor = 1;
+        myled3 = 1;
+        wait(2);
+        motor = 0;
+        myled3 = 0;
+        emg.attach(&emg_isr, .001);
+        
+    }
+
 }
 
 int maxArr(int start, int end, int length){
@@ -84,6 +112,9 @@ int classify(int start, int end, int length) {
     //pc.printf("start:%d       End:%d   \r\n",start, end); 
     //pc.printf("entropy: %f\r\nmax:%d\r\nmin:%d\r\ndistance:%d\r\n", entropy, max, min, distance);
     //return 1;
+    if(max < 2320){
+        return 0;
+    }
     if(entropy <= 6.505481){
         return 0;
     }else{
@@ -130,6 +161,9 @@ int main() {
     int displacement = 125;
     int classification;
     int cur_position = window;
+    int flag = 0;
+    int count = 0;
+    int count_length = CHEW_BUFFER_LENGTH/2; // wait for half of the buffer to refill before checking
     while(1) {
         if((cur_position == EMG_BUFFER_LENGTH && emg_position < displacement) || emg_position >= cur_position){
             //pc.printf("%d\r\n", cur_position);
@@ -137,7 +171,8 @@ int main() {
             classification = classify(cur_position - window , cur_position, window);
             myled2 = 0;
             myled = classification;
-            motor = classification;
+            chew_place(classification);
+            //motor = classification;
             //pc.printf("p:%d\r\n", cur_position);
             //pc.printf("c:%d\r\n", classification);
             cur_position = (cur_position + displacement) /*% EMG_BUFFER_LENGTH*/;
@@ -145,6 +180,17 @@ int main() {
                 //ignore overlap of end of buffer and begining of buffer
                 cur_position = window;
             }
+            if(count > count_length){
+                flag = 1;    
+            }else{
+                count++;    
+            }
+            
+        }else if(flag) {
+            //inbetween adding classifications, check chew_buffer
+            chew_test();
+            flag = 0; 
+            count = 0;
         }
     }
 }
